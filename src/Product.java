7e05e99eec44d2149ffbce007ac4ecf23d1bfada
LauraMJ <lauramjphelan@gmail.com>
2014-08-12 18:08:01 +0100

    100th attempt trying to commit

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
+//Why is this not updating
 
 public class Product {
 	private String productID;
@@ -6,7 +9,7 @@ public class Product {
 	private double markup;
 	private Supplier supplier;
 
-	public Product(String product, String name, double cost, double markup, Supplier supplier) {
+	public Product(String productID, String name, double cost, double markup, Supplier supplier) {
 		this.productID = productID;
 		this.name = name;
 		this.cost = cost;
@@ -55,24 +58,31 @@ public class Product {
 		this.supplier = supplier;
 	}
 	
-	public void addProductToList(Product product){
+	public void addProductToList(Product product, ArrayList<Product> products){
 		//Will take in a product and add it to the products list in the driver class
+		for(Product p: products){
+			if(p.getProductID() == product.getProductID()){
+				System.out.println("Product is already in product list");
+			}else{
+				products.add(p);
+			}
+		}
 	}
 	
-	public void removeProductFromList(Product product){
+	public void removeProductFromList(Product product, ArrayList<Product> products){
 		//Will remove a product from the products list in the driver class
+		products.remove(product);
 	}
-	
-	public void editProduct(Product product){
-		//Not valid - comments?
-	}
-	
+		
 	public void viewProduct(Product product){
 		//Temp println in place of GUI
 		System.out.println("ID: "+product.getProductID()+"\nName: "+product.getName()+"\nCost: "+product.getCost()+"\nMarkup: "+product.getMarkup()+"\nSupplier: "+product.getSupplier());
 	}
 
-	public void viewProductList(){
+	public void viewProductList(ArrayList<Product> products){
 		//Will list products from product list in driver class
+		for(Product product: RetailSystem.getInstance().getProducts()){
+			System.out.println("ID: "+product.getProductID()+"\nName: "+product.getName()+"\nCost: "+product.getCost()+"\nMarkup: "+product.getMarkup()+"\nSupplier: "+product.getSupplier());
+		}
 	}
 }
