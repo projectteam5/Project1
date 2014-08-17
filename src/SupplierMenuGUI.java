81479f399a74dc72c2755c2c57edc6f3d4f58d6d
megagyger <mcgowancalvin@ymail.com>
2014-08-17 17:33:06 +0100

    Supplier menu updated as per the spec on friday

---
+++ src/SupplierMenuGUI.java
@@ -0,0 +1,72 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class SupplierMenuGUI extends JFrame{
+
+	public SupplierMenuGUI() {
+		setTitle("SUPPLIER GUI");
+		setSize(400,400);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		JButton editButton = new JButton("Edit Supplier");
+		JButton viewListButton = new JButton("List Suppliers");
+		JButton deleteButton = new JButton("Delete Supplier");
+		JButton addButton = new JButton("Add Supplier");
+		JButton productPerSupplier = new JButton("ViewSupplierOfProduct");
+		
+		// Launches the edit supplier frame
+		editButton.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				EditSupplierGUI editSupplier =new EditSupplierGUI();
+			}
+		});
+		
+		addButton.addActionListener(new ActionListener() {		
+			public void actionPerformed(ActionEvent arg0) {
+				AddSupplierGUI addSupplierGUI = new AddSupplierGUI();
+			}
+		});
+		
+		
+		// Launches the add/delete supplier from list frame
+		deleteButton.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				DeleteSupplierGUI deleteSupplierGUI = new DeleteSupplierGUI();
+			}
+		});
+		
+		// Launches the list suppliers frame
+		viewListButton.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				ListSuppliersGUI listSupplierGUI = new ListSuppliersGUI();		
+			}
+		});
+		
+		// Launches the the supplier of a particular product frame
+		productPerSupplier.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				SupplierOfProductGUI supplierOfProductGUI = new SupplierOfProductGUI();
+			}
+		});
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(0,1));
+		panel.add(editButton);
+		panel.add(deleteButton);
+		panel.add(addButton);
+		panel.add(productPerSupplier);
+		panel.add(viewListButton);
+		Container cp = getContentPane();
+		cp.add(panel);
+		setVisible(true);
+	}
+}
