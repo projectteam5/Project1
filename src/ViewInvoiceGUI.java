1a2f99412474f5451642d2d03e6e9cf27f3c7367
LauraMJ <lauramjphelan@gmail.com>
2014-08-28 18:11:32 +0100

    Populating lineItems and not duplicating items on second press. Need to remove last column of table

--- src/ViewInvoiceGUI.java
+++ src/ViewInvoiceGUI.java
@@ -1,3 +1,5 @@
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Vector;
@@ -8,9 +10,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.TableModel;
+
 
 
 public class ViewInvoiceGUI extends JPanel{
+	private TableModel dataModel;
 	private JLabel invoiceLabel;
 	private JComboBox<String> invoiceDropDown = new JComboBox<String>();
 	private JButton buttonViewInvoice;
@@ -18,29 +23,46 @@ public class ViewInvoiceGUI extends JPanel{
 	private String selectedInvoiceID;
 	private Invoice chosenInvoice;
 	//For showing invoice details
-	private JPanel showInvoicePanel;
-	private JScrollPane scrollPaneInvoices;
+	private JLabel invoiceInfoLabel1;
+	private JLabel invoiceInfoLabel2;
+/*	private JPanel showInvoicePanel;
+	private JScrollPane scrollPaneInvoice;
 	private Vector<Invoice> vet;
 	private JTable table;
-	private Vector<Invoice> vet2;
+	private Vector<Invoice> vet2;*/
 	//For showing lineItems
 	private Vector<LineItem> vet3;
 	private JTable table2;
 	private JScrollPane scrollPaneLineItems;
+	private boolean populated = false;
 
 
 	public ViewInvoiceGUI() {
 		invoiceLabel = new JLabel("Please choose an invoice from the list below");
 		buttonViewInvoice = new JButton("View Invoice");
 		invoiceDetails = new JLabel("Invoice Details:");
+		invoiceInfoLabel1 = new JLabel("");
+		invoiceInfoLabel2 = new JLabel("");
+		
+/*		vet = new Vector<Invoice>();
+		dataModel = new InvoiceTable(vet);
+		table = new JTable(dataModel);
+		scrollPaneInvoice = new JScrollPane(table);*/
+		
+		vet3 = new Vector<LineItem>();
+		dataModel = new LineItemTable(vet3);
+		table2 = new JTable(dataModel);
+		scrollPaneLineItems = new JScrollPane(table2);
 		
 		Invoice.invoiceListComplete(invoiceDropDown);
 		this.add(invoiceLabel);
 		this.add(invoiceDropDown);
 		this.add(buttonViewInvoice);
 		this.add(invoiceDetails);
-		
-		//this.setVisible(true);
+		//this.add(scrollPaneInvoice);
+		this.add(invoiceInfoLabel1);
+		this.add(invoiceInfoLabel2);
+		this.add(scrollPaneLineItems);
 		
 		buttonViewInvoice.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -50,13 +72,37 @@ public class ViewInvoiceGUI extends JPanel{
 				for(Invoice invoice: RetailSystem.getInstance().getInvoices()){
 					if(selectedInvoiceID.equalsIgnoreCase(invoice.getInvoiceID())){
 						chosenInvoice = invoice;
-						System.out.println("1."+selectedInvoiceID+" 2."+invoice.getInvoiceID());
+						//System.out.println("1."+selectedInvoiceID+" 2."+invoice.getInvoiceID());
 					}
 				}
-				//Show information from invoice in one table
+				//Show information from invoice in a label
+				//showInvoiceInformation(chosenInvoice);
+				invoiceInfoLabel1.setText("Invoice Date: "+chosenInvoice.getInvoiceDate()+" | Invoice ID: "+chosenInvoice.getInvoiceID());
+				invoiceInfoLabel2.setText("Customer Name: "+chosenInvoice.getCustomer().getName()+" | Customer ID: "+chosenInvoice.getCustomer().getCustomerID()+" | Total Cost: €"+chosenInvoice.getTotalInvoice());
 				//Show information from Invoice.Sale.LineItems in another just below
+				if(!populated){
+					showLineItemInformation(chosenInvoice);
+				}
 			}
 		});
+	}
+	
+/*	public void showInvoiceInformation(Invoice invoice){
+		vet.add(invoice);
+		repopulate();
+	}*/
+	
+	public void showLineItemInformation(Invoice invoice){
+		for(LineItem lineItem: invoice.getSale().getLineItems()){
+			vet3.add(lineItem);
+			repopulate();
+			populated = true;
+		}
+	}
+	
+	public void repopulate() {
+		table2.revalidate();
+		table2.repaint();
 	}
 
 
