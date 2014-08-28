c9dc917d8c3bc3d0333118402142d98991883774
ColmDillon <colmdillon89@gmail.com>
2014-08-28 18:17:01 +0100

    Stock list wont display dates think my stock table class is the problem

--- src/StockTable.java
+++ src/StockTable.java
@@ -1,3 +1,4 @@
+import java.text.SimpleDateFormat;
 import java.util.Vector;
 
 import javax.swing.table.AbstractTableModel;
@@ -5,10 +6,12 @@ import javax.swing.table.AbstractTableModel;
 
 public class StockTable extends AbstractTableModel {
 	private Vector<Stock> v = null;
+	private Vector<Order> v2 = null;
 	private String[] colNames = {"Product","Units","Orders expected"};
 	
-	public StockTable(Vector<Stock> v) {
+	public StockTable(Vector<Stock> v,Vector<Order> v2) {
 		this.v = v;
+		this.v2 = v2;
 	}
 	
 	public int getColumnCount(){
@@ -19,7 +22,9 @@ public class StockTable extends AbstractTableModel {
 	}
 	public String getValueAt(int row, int col){
 		Stock stock = (Stock)v.elementAt(row);
+		Order order = (Order)v2.elementAt(row);
 		String val = null;
+		SimpleDateFormat dt = new SimpleDateFormat("dd/MM/yyyy");
 		switch(col){
 		case 0: 
 			val = stock.getProduct().getName();
@@ -27,14 +32,12 @@ public class StockTable extends AbstractTableModel {
 		case 1: 
 			val = stock.getUnits()+"";
 			break;
-		/*case 2:
-			for(Order o:RetailSystem.getInstance().getOrders()){
-				if(stock.getProduct().getName().equalsIgnoreCase(o.getProduct().getName())){
-					val = o.getExpectedDeliveryDate()+"";
-				}else{
-					val = "";
-				}
-			}*/
+		case 2:
+			if(stock.getProduct().getProductID().equalsIgnoreCase(order.getProduct().getProductID())){
+				val = dt.format(order.getExpectedDeliveryDate());
+			}else{
+				val = "";
+			}
 		
 		default: 
 			val = "";
